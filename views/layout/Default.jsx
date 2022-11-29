14007c6ab6dc818607c9484ac3ab8368bf15ca41
boldPro <probold777@gmail.com>
2022-11-29 13:53:51 -0500

    added react-dom to json

--- views/layout/Default.jsx
+++ views/layout/Default.jsx
@@ -7,7 +7,7 @@ class DefaultLayout extends React.Component{
             <html>
                 <head>
                     <title>{this.props.title}</title>
-                    <link rel="stylesheet" href="/css/app.css" /> 
+                    <link rel="stylesheet" href="/css/app.css" /> {/*Here we don't have to give a path to  public folder */}
                 </head>
                 <body>
                     <h1>{this.props.title}</h1>
